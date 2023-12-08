b708f0273b2976cbd24aa3e37523d75e79a69084
VickyPawar7474 <vickyrajpawar44@gmail.com>
2023-12-08 19:22:25 +0530

    Navigation creation with assign and replace

---
+++ navigation.js
@@ -0,0 +1,14 @@
+
+function assignFun(){
+    window.location.assign("https://www.facebook.com/")
+
+}
+
+
+
+function repleceFun(){
+    window.location.replace("https://www.facebook.com/")
+
+}
+
+
